7c91f2443ee3ba0389325e212488eb14f95eebf9
floyse-back <ffloyse@gmail.com>
2025-05-07 16:42:50 +0300

    Create Logger

--- app/infrastructure/logger/logger.py
+++ app/infrastructure/logger/logger.py
@@ -1,31 +1,33 @@
 import logging
 
+from app.domain.logger import ILogger
 
 
-class Logger:
+class Logger(ILogger):
     def __init__(self,name=__name__,level=logging.INFO):
         self.logger = logging.getLogger(name)
+        self.logger_config(level)
 
     def logger_config(self,level):
         logging.basicConfig(level=level,
                             datefmt="%Y:%m:%d %H:%M:%S",
-                            format="%(asctime)s - %(module)s:(lineno)%s - %(levelname)s - %(message)s)"
+                            format="%(asctime)s - %(module)s:(lineno)%d  - %(levelname)s - %(message)s)"
                             )
 
-    async def info(self,msg,args,**kwargs):
-        self.logger.info(msg,args **kwargs)
+    def info(self,msg,*args,**kwargs):
+        self.logger.info(msg,*args, **kwargs)
 
-    async def error(self,msg,args,**kwargs):
-        self.logger.error(msg,args,**kwargs)
+    def error(self,msg,*args,**kwargs):
+        self.logger.error(msg,*args,**kwargs)
 
-    async def warning(self,msg,args,**kwargs):
-        self.logger.warning(msg,args,**kwargs)
+    def warning(self,msg,*args,**kwargs):
+        self.logger.warning(msg,*args,**kwargs)
 
-    async def critical(self,msg,args,**kwargs):
-        self.logger.critical(msg,args,**kwargs)
+    def critical(self,msg,*args,**kwargs):
+        self.logger.critical(msg,*args,**kwargs)
 
-    async def debug(self,msg,args,**kwargs):
-        self.logger.debug(msg,args,**kwargs)
+    def debug(self,msg,*args,**kwargs):
+        self.logger.debug(msg,*args,**kwargs)
 
-    async def exception(self,msg,args,**kwargs):
-        self.logger.exception(msg,args,**kwargs)
+    def exception(self,msg,*args,**kwargs):
+        self.logger.exception(msg,*args,**kwargs)
--- app/infrastructure/logging/logger.py
+++
@@ -1,5 +0,0 @@
-
-
-
-class Logger():
-    pass
